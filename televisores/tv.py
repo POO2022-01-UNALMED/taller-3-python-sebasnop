43aa1909c1a35bfcc650ecca9ca6f8daab25cfc2
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-03-27 15:12:27 -0500

    Primer intento

---
+++ televisores/tv.py
@@ -0,0 +1,75 @@
+class TV:
+    _canal = 1
+    _volumen = 1
+    _precio = 500
+    _numTV = 0
+
+    def __init__ (self, marca, estado):
+        self._marca = marca
+        self._estado = estado
+        TV._numTV =+ 1
+    
+    def setMarca(self, marca):
+        self._marca = marca
+    
+    def getMarca(self):
+        return self._marca
+
+    def setControl(self, control):
+        self._control = control
+    
+    def getControl(self):
+        return self._control
+    
+    def setPrecio(self, precio):
+        self._precio = precio
+    
+    def getPrecio(self):
+        return self._precio
+    
+    def setVolumen(self, volumen):
+        if (volumen >= 0 and volumen <= 7 and self._estado):
+            self._volumen = volumen
+    
+    def getVolumen(self):
+        return self._volumen
+    
+    def setCanal(self, canal):
+        if (canal >= 1 and canal <= 120 and self._estado):
+            self._canal = canal
+    
+    def getCanal(self):
+        return self._canal
+
+    @classmethod
+    def setNumTV(cls, numTV):
+        cls._numTV = numTV
+    
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV
+    
+    def turnOn(self):
+        self._estado = True
+    
+    def turnOff(self):
+        self._estado = False
+    
+    def getEstado(self):
+        return self._estado
+    
+    def canalUp(self):
+        if (self._canal < 120 and self._estado):
+            self._canal =+ 1
+    
+    def canalDown(self):
+        if (self._canal > 1 and self._estado):
+            self._canal =- 1
+    
+    def volumenUp(self):
+        if (self._volumen < 7 and self._estado):
+            self._volumen =+ 1
+    
+    def volumenDown(self):
+        if (self._volumen > 0 and self._estado):
+            self._volumen =- 1
