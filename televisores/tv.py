5d3b529966ee106ba21724071eab8ad4525d30d5
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-03-27 15:23:52 -0500

    Se arreglaron los operadores de suma y resta a los modificadores de numTV, volumen y canal de tv

--- televisores/tv.py
+++ televisores/tv.py
@@ -7,7 +7,7 @@ class TV:
     def __init__ (self, marca, estado):
         self._marca = marca
         self._estado = estado
-        TV._numTV =+ 1
+        TV._numTV += 1
     
     def setMarca(self, marca):
         self._marca = marca
@@ -60,16 +60,16 @@ class TV:
     
     def canalUp(self):
         if (self._canal < 120 and self._estado):
-            self._canal =+ 1
+            self._canal += 1
     
     def canalDown(self):
         if (self._canal > 1 and self._estado):
-            self._canal =- 1
+            self._canal -= 1
     
     def volumenUp(self):
         if (self._volumen < 7 and self._estado):
-            self._volumen =+ 1
+            self._volumen += 1
     
     def volumenDown(self):
         if (self._volumen > 0 and self._estado):
-            self._volumen =- 1
+            self._volumen -= 1
