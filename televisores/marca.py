b4f647c7a64c48b2ea632bdb14668358d81923ef
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-03-27 15:32:25 -0500

    Se corrigen getters y setters de Marca, agregandoles como atributo el self

--- televisores/marca.py
+++ televisores/marca.py
@@ -2,8 +2,8 @@ class Marca:
     def __init__(self, nombre):
         self._nombre = nombre
     
-    def setNombre(nombre):
+    def setNombre(self, nombre):
         self._nombre = nombre
     
-    def getNombre():
+    def getNombre(self):
         return self._nombre
